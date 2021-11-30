85579cff26f0a46237c49a41098ad50f0681ee3a
Ribert Rodriguez <ribert_rodriguez21@hotmail.com>
2021-11-30 16:46:14 -0500

    1

---
+++ Covid.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 16:42:27 2021
+
+@author: riber
+"""
+
+import pandas as pd
+
+url = 'cov_22.csv'
+data = pd.read_csv(url)
+
+data['Nombre departamento'].replace('BOGOTA','CUNDINAMARCA', inplace=True)
+data['Nombre departamento'].replace('BARRANQUILLA','ATLANTICO', inplace=True)
+data['Ubicación del caso'].replace('CASA','Casa', inplace=True)
+data['Ubicación del caso'].replace('casa','Casa', inplace=True)
+data['Recuperado'].replace('fallecido','Fallecido', inplace=True)
+data['Sexo'].replace('f','F', inplace=True)
+data['Sexo'].replace('m','M', inplace=True)
